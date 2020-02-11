d43b39d4b581a837011a07510b6b517cb9e00229
Jean Delmas <44100908+lazyjd@users.noreply.github.com>
2020-02-11 13:08:29 -0500

    Delete test.sh

--- test.sh
+++
@@ -1,3 +0,0 @@
-curl -s https://www.backmarket.fr/iphone-x-64-go-gris-sideral-debloque-tout-operateur-pas-cher/36833.html | grep -E -o "price_with_currency.\".{0,6}"
-echo "END"
-curl -s https://www.backmarket.fr/iphone-x-64-go-argent-debloque-tout-operateur-pas-cher/36835.html | grep -E -o "price_with_currency.\".{0,6}"
