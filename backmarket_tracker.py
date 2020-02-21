b8843ee567a0d96c99543a40df1b7181266ad32e
lazY <jean.delmas.pro@gmail.com>
2020-02-21 19:19:45 +0100

    Automatic Curency Detection
    Based on the url, detect the currency of the backmarket version

--- backmarket_tracker.py
+++ backmarket_tracker.py
@@ -13,22 +13,55 @@ Made by 0xlazY !
 import requests, re
 #from notify_run import Notify ## TODO
 
+def get_currency(url):
+
+    currency_dic = {
+        '€': ['fr', 'es', 'it', 'de'],
+        '$': ['com'],
+        '£': ['uk'] ## co.uk
+    }
+
+    splited_url = url.split('.')
+    
+    if splited_url[3] == 'uk':
+        splited_url = 'uk'
+    else:
+        splited_url = splited_url[2].split("/")[0]
+    #print(splited_url) ## debug
+
+    currency_symbole = "$" ## default, if failed
+    
+    for key, value in currency_dic.items():
+        if splited_url in value:
+            currency_symbole = key
+
+    #print(currency_symbole) ##Debug
+    return currency_symbole
+
+
 def get_webcontent(url):
 
     page = requests.get(url)
     content = page.content.decode()
+    currency_symbole = get_currency(url)
 
-    raw_prices = re.findall("price_with_currency.{0,10}€", content) # Parse the entire WebPage, search 'price_with_currency*€'
+    pattern = 'price_with_currency.\".{0,9}.\"'
+    raw_prices = re.findall(pattern, content) # Parse the entire WebPage, search 'price_with_currency*€'
+    print(raw_prices) ## debug
 
     price_lst = []
 
     for price in raw_prices:
         try: # because of potential problems when loading webpages and inconsistency, use 'try' before any index reference ([.])
-            parsed_price = float(price.strip().split('"')[1].split('\xa0')[0].replace(',', '.')) # get rid of all the junk
+            if currency_symbole == '€':
+                parsed_price = float(price.strip().split('"')[1].split('\xa0')[0].replace(',', '.')) # get rid of all the junk
+            else:
+                parsed_price = float(price.strip().split('"')[1].replace(currency_symbole, ''))
+
             if parsed_price > false_positive_price:
                 price_lst.append(parsed_price) # add the formated price to the price_lst
         except:
-            continue
+            print("Problem ! {}".format(price))
     
     return price_lst
 
@@ -59,21 +92,22 @@ def main():
     for url in url_lst:
         #print(get_webcontent(url))
         price_lst = get_webcontent(url)
-        alerter(price_lst)
+        #alerter(price_lst)g
+        print(price_lst)
 
 
 if __name__ == '__main__':
-    url_lst = ['https://www.backmarket.fr/iphone-x-64-go-gris-sideral-debloque-tout-operateur-pas-cher/36833.html']
+    url_lst = ['https://www.backmarket.fr/iphone-8-64-go-gris-sideral-debloque-tout-operateur-pas-cher/36827.html']
     device_name = 'iPhone X'
     # i.e, iPhone X 64gb Black
 
-    currency_symbole = '€' # TODO add other currency support, $, £ ...
+    currency_symbole = '$' # TODO add other currency support, $, £ ...
 
     notify_run_url = get_notify_run_url('config.cfg')
     # Create a notify_run channel and replace the url in config file (https://notify.run)
     ## TODO add channel creation with notidy_run module
 
-    false_positive_price = 20 # Impossible price for the specified product
+    false_positive_price = 100 # Impossible price for the specified product
     price_wanted = 450 # Price of the product below which you want to receive a notification
 
     main()
